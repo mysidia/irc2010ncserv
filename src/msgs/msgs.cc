b74d55629cf1536e41361e4f1479dec84cb34f2d
Mysidia <69487+mysidia@users.noreply.github.com>
2001-09-27 00:18:37 +0000

    leftover changes

--- src/msgs/msgs.cc
+++ src/msgs/msgs.cc
@@ -18,6 +18,23 @@
   *  any longer
   */
 
+// We need a table of function -> token mappings
+// Secondly, the parser should call the proper function given
+// a token is incoming.
+
+// The parameters the (*ccParse) pointer is called with should
+// be independent of the underlying protocol
+
+struct MessageTokenTable
+{
+	CommandToken		tok;
+	ParserFunctionPtr	ccParse;
+}
+tokens[]
+=
+{
+};
+
 #if 0
 static void vsendto_one( aClient *target, const char *fmt, va_list ap)
 {
